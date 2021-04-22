b36bf4d7a4d13286ac1c951489a5381746e6fc0a
Rahul Gupta <guptarahul70322@gmail.com>
2021-04-22 12:29:40 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
+import ForgotPassword from './components/ForgotPassword';
+import UpdateProfile from './components/UpdateProfile';
 function App() {
   return (
     <Container
@@ -24,7 +26,17 @@ function App() {
             <Switch>
               <Route path="/signup" exact component={Signup}></Route>
               <PrivateRoute path="/" exact component={Dashboard}></PrivateRoute>
+              <PrivateRoute
+                path="/update-profile"
+                exact
+                component={UpdateProfile}
+              ></PrivateRoute>
               <Route path="/login" exact component={Login}></Route>
+              <Route
+                path="/forgot-password"
+                exact
+                component={ForgotPassword}
+              ></Route>
             </Switch>
           </AuthProvider>
         </BrowserRouter>
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -16,6 +16,15 @@ export function AuthProvider({ children }) {
   function logout() {
     return auth.signOut();
   }
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
@@ -28,6 +37,9 @@ export function AuthProvider({ children }) {
     signup,
     login,
     logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
   };
   return (
     <AuthContext.Provider value={value}>
