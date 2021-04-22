d08ab6c75ae5c22d4201f078cc226fa2ceab0106
Rahul Gupta <guptarahul70322@gmail.com>
2021-04-22 11:39:19 +0530

    private route for preventing access to dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import { Container } from 'react-bootstrap';
 import Signup from './components/Signup';
 import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex align-items-center justify-content-center"
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <div
+        className="w-100"
         style={{
-          minHeight: '100vh',
+          maxWidth: '400px',
         }}
       >
-        <div
-          className="w-100"
-          style={{
-            maxWidth: '400px',
-          }}
-        >
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+        <BrowserRouter>
+          <AuthProvider>
+            <Switch>
+              <Route path="/signup" exact component={Signup}></Route>
+              <PrivateRoute path="/" exact component={Dashboard}></PrivateRoute>
+              <Route path="/login" exact component={Login}></Route>
+            </Switch>
+          </AuthProvider>
+        </BrowserRouter>
+      </div>
+    </Container>
   );
 }
 
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,6 +10,12 @@ export function AuthProvider({ children }) {
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
   }
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+  function logout() {
+    return auth.signOut();
+  }
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
@@ -20,9 +26,12 @@ export function AuthProvider({ children }) {
   const value = {
     currentUser,
     signup,
+    login,
+    logout,
   };
   return (
     <AuthContext.Provider value={value}>
+      {/* {!loading && <p>{currentUser.email}</p>} */}
       {!loading && children}
     </AuthContext.Provider>
   );
